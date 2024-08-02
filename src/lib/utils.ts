6d811af23525691a20d8c33f12b91a9ace8afca3
Felipe Sobrinho <felipe.sobrinho.3@outlook.com>
2024-08-02 17:07:26 -0300

    add: shadcnui and skeleton component to loading

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(price: number | null) {
+  if (!price) return "R$ 0,00";
+
+  return new Intl.NumberFormat("pt-BR", {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(price/100);
+}
